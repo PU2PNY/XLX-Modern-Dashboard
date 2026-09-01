ee22b6735340876b55acf41c054b29d618baafc6
PU2PNY <47339656+PU2PNY@users.noreply.github.com>
2026-09-01 07:38:03 -0300

    feat: add support page interactions

---
+++ assets/support-native.js
@@ -0,0 +1,11 @@
+'use strict';
+(() => {
+  document.querySelectorAll('.support-page a[href^="#"]').forEach(link => {
+    link.addEventListener('click', event => {
+      const target = document.querySelector(link.getAttribute('href'));
+      if (!target) return;
+      event.preventDefault();
+      target.scrollIntoView({behavior:'smooth', block:'start'});
+    });
+  });
+})();
